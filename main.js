70c7e1f0146a25fc0dfb8d737ecdfc943da14db3
WaggishMail07 <163008966+WaggishMail07@users.noreply.github.com>
2024-06-03 20:04:48 +0000

    commit it

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+console.log("hello world")
+const canvas = document.getElementById('canvas1');
+const cxt = canvas.getContext('2d');
+console.log(ctx);
+
+const CANVAS_WIDTH = canvas.width = 600;
